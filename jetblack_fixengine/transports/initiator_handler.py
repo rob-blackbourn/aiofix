576e093ce0dfcaa47710afe68b32fbd7fde8a63c
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-01-23 12:33:12 +0000

    Removed unwanted code

--- jetblack_fixengine/transports/initiator_handler.py
+++ jetblack_fixengine/transports/initiator_handler.py
@@ -2,14 +2,13 @@
 
 from abc import ABCMeta, abstractmethod
 import asyncio
-from datetime import datetime, time, tzinfo, timezone
+from datetime import datetime, timezone
 import logging
-from typing import Awaitable, Callable, Mapping, Any, Optional, Tuple, cast
+from typing import Awaitable, Callable, Mapping, Any, Optional
 
 from jetblack_fixparser.fix_message import FixMessageFactory, FixMessage
 from jetblack_fixparser.meta_data import ProtocolMetaData
 from ..types import Store, Event
-from ..utils.date_utils import wait_for_time_period
 from ..utils.json_utils import dict_to_json
 
 from .initiator_state import (
@@ -20,13 +19,6 @@ from .initiator_state import (
 )
 
 LOGGER = logging.getLogger(__name__)
-
-STATE_DISCONNECTED = 'disconnected'
-STATE_CONNECTED = 'connected'
-STATE_LOGGING_ON = 'logon.start'
-STATE_LOGGED_ON = 'logon.ok'
-STATE_LOGGING_OFF = 'logout.start'
-STATE_LOGGED_OUT = 'logout.done'
 
 EPOCH_UTC = datetime.fromtimestamp(0, timezone.utc)
 
@@ -43,24 +35,19 @@ class InitiatorHandler(metaclass=ABCMeta):
             heartbeat_timeout: int,
             cancellation_event: asyncio.Event,
             *,
-            heartbeat_threshold: int = 1,
-            logon_time_range: Optional[Tuple[time, time]] = None,
-            tz: Optional[tzinfo] = None
+            heartbeat_threshold: int = 1
     ) -> None:
         self.heartbeat_timeout = heartbeat_timeout
         self.heartbeat_threshold = heartbeat_threshold
         self.cancellation_event = cancellation_event
-        self.logon_time_range = logon_time_range
-        self.tz = tz
         self.fix_message_factory = FixMessageFactory(
             protocol,
             sender_comp_id,
             target_comp_id
         )
 
-        self._state = STATE_DISCONNECTED
-        self._last_send_time_utc: datetime = EPOCH_UTC
-        self._last_receive_time_utc: datetime = EPOCH_UTC
+        self._last_send_time: datetime = EPOCH_UTC
+        self._last_receive_time: datetime = EPOCH_UTC
         self._store = store
         self._session = self._store.get_session(sender_comp_id, target_comp_id)
         self._send: Optional[Callable[[Event], Awaitable[None]]] = None
@@ -80,7 +67,6 @@ class InitiatorHandler(metaclass=ABCMeta):
             self,
             _fix_message: Optional[FixMessage]
     ) -> Optional[AdminEventType]:
-        self._state = STATE_LOGGING_ON
         await self.send_message(
             'LOGON',
             {
@@ -157,23 +143,23 @@ class InitiatorHandler(metaclass=ABCMeta):
     async def _set_incoming_seqnum(self, seqnum: int) -> None:
         await self._session.set_incoming_seqnum(seqnum)
 
-    async def _send_event(self, event: Event, send_time_utc: datetime) -> None:
+    async def _send_event(self, event: Event, timestamp: datetime) -> None:
         if self._send is None:
             raise ValueError('Not connected')
         await self._send(event)
-        self._last_send_time_utc = send_time_utc
+        self._last_send_time = timestamp
 
     async def send_message(
             self,
             msg_type: str,
             message: Optional[Mapping[str, Any]] = None
     ) -> None:
-        send_time_utc = datetime.now(timezone.utc)
+        timestamp = datetime.now(timezone.utc)
         msg_seq_num = await self._next_outgoing_seqnum()
         fix_message = self.fix_message_factory.create(
             msg_type,
             msg_seq_num,
-            send_time_utc,
+            timestamp,
             message
         )
         LOGGER.info(
@@ -185,7 +171,7 @@ class InitiatorHandler(metaclass=ABCMeta):
             'type': 'fix',
             'message': fix_message.encode(regenerate_integrity=True)
         }
-        await self._send_event(event, send_time_utc)
+        await self._send_event(event, timestamp)
 
     async def logout(self) -> None:
         """Send a logout message.
@@ -199,10 +185,10 @@ class InitiatorHandler(metaclass=ABCMeta):
             test_req_id (Optional[str], optional): An optional test req id.
                 Defaults to None.
         """
-        body_kwargs = {}
+        message = {}
         if test_req_id:
-            body_kwargs['TestReqID'] = test_req_id
-        await self.send_message('HEARTBEAT', body_kwargs)
+            message['TestReqID'] = test_req_id
+        await self.send_message('HEARTBEAT', message)
 
     @abstractmethod
     async def on_application_message(self, message: Mapping[str, Any]) -> bool:
@@ -232,18 +218,17 @@ class InitiatorHandler(metaclass=ABCMeta):
 
             await self._session.save_message(event['message'])
 
-            msgcat = cast(str, fix_message.meta_data.msgcat)
-            if msgcat == 'admin':
+            if fix_message.meta_data.msgcat == 'admin':
                 event_type = AdminEvent.to_event_type(fix_message)
                 await self._state_machine.handle_event(event_type, fix_message)
                 status = self._state_machine != AdminState.STOP
             else:
                 status = await self.on_application_message(fix_message.message)
 
-            msg_seq_num: int = cast(int, fix_message.message['MsgSeqNum'])
+            msg_seq_num: int = fix_message.message['MsgSeqNum']
             await self._set_incoming_seqnum(msg_seq_num)
 
-            self._last_receive_time_utc = datetime.now(timezone.utc)
+            self._last_receive_time = datetime.now(timezone.utc)
 
             return status
 
@@ -256,24 +241,26 @@ class InitiatorHandler(metaclass=ABCMeta):
             return False
 
     async def _handle_heartbeat(self) -> float:
-        now_utc = datetime.now(timezone.utc)
-        seconds_since_last_send = (
-            now_utc - self._last_send_time_utc
-        ).total_seconds()
-        if seconds_since_last_send >= self.heartbeat_timeout and self._state == STATE_LOGGED_ON:
+        timestamp = datetime.now(timezone.utc)
+        elapsed = (timestamp - self._last_send_time).total_seconds()
+        if (
+                elapsed >= self.heartbeat_timeout and
+                self._state_machine.state == AdminState.PENDING
+        ):
             await self.heartbeat()
-            seconds_since_last_send = 0
+            elapsed = 0
 
-        return self.heartbeat_timeout - seconds_since_last_send
+        return self.heartbeat_timeout - elapsed
 
     async def _handle_timeout(self) -> None:
-        if not self._state == STATE_LOGGED_ON:
+        if not self._state_machine.state != AdminState.PENDING:
             return
 
-        now_utc = datetime.now(timezone.utc)
-        seconds_since_last_receive = (
-            now_utc - self._last_receive_time_utc).total_seconds()
-        if seconds_since_last_receive - self.heartbeat_timeout > self.heartbeat_threshold:
+        timestamp = datetime.now(timezone.utc)
+        elapsed = (
+            timestamp - self._last_receive_time
+        ).total_seconds()
+        if elapsed - self.heartbeat_timeout > self.heartbeat_threshold:
             await self.send_message(
                 'TEST_REQUEST',
                 {
@@ -289,20 +276,6 @@ class InitiatorHandler(metaclass=ABCMeta):
     async def on_logout(self, message: Optional[Mapping[str, Any]]) -> None:
         ...
 
-    async def _wait_till_logon_time(self) -> Optional[datetime]:
-        if self.logon_time_range:
-            start_time, end_time = self.logon_time_range
-            LOGGER.info('Logon from %s to %s', start_time, end_time)
-            end_datetime = await wait_for_time_period(
-                datetime.now(tz=self.tz),
-                start_time,
-                end_time,
-                cancellation_event=self.cancellation_event
-            )
-            return end_datetime
-
-        return None
-
     async def __call__(
             self,
             send: Callable[[Event], Awaitable[None]],
@@ -314,7 +287,6 @@ class InitiatorHandler(metaclass=ABCMeta):
 
         if event['type'] == 'connected':
             LOGGER.info('connected')
-            self._state = STATE_CONNECTED
             await self._state_machine.handle_event(AdminEventType.CONNECTED, None)
 
             ok = True
@@ -339,4 +311,3 @@ class InitiatorHandler(metaclass=ABCMeta):
             raise RuntimeError('Failed to connect')
 
         LOGGER.info('disconnected')
-        self._state = STATE_DISCONNECTED
